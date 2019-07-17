d468dde814595bb79db19f930fdc4e0942952d62
Derek S <dschwan21@me.com>
2019-07-17 15:40:17 -0700

    built three helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,13 +9,31 @@ module.exports = {
   remove
 };
 
-function find() {}
+function find() {
+  return db("schemes");
+}
 
-function findById() {}
+function findById(id) {
+  return db("schemes")
+    .where({ id }) // .where always returns an array
+    .first()
+    .then(scheme => {
+      if (scheme) {
+        return scheme;
+      } else {
+        return null;
+      }
+    });
+}
 
-function findSteps() {}
+function findSteps(id) {
+  return db("schemes")
+    .join("steps", "schemes.id", "steps.scheme_id")
+    .where({ scheme_id: id })
+    .orderBy("steps.step_number", "asc");
+}
 
-function add() {}
+function add(scheme) {}
 
 function update() {}
 
